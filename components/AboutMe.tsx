b775cf005a45172fb72f155d408b9f5e71c941be
dillon-cleaver <dillon.cleaver@gmail.com>
2025-03-23 13:30:36 -0500

    Add assets directory and move profile photo.

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import styles from "./AboutMe.module.css";
+import profilePic from "@/assets/dillon-sept-23.jpg";
 
 export default function AboutMe() {
   return (
@@ -11,26 +12,31 @@ export default function AboutMe() {
           <div className={styles.text}>
             <h1 className={styles.title}>Frontend Developer</h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              euismod, nisl vel ultricies lacinia, nisl nisl aliquam nisl, eget
-              aliquam nisl nisl sit amet nisl. Sed euismod, nisl vel ultricies
-              lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl sit amet
-              nisl.
+              Frontend Developer with a strong focus on creating engaging,
+              cross-platform mobile applications for iOS, Android, and web.
+              Proficient in React Native, React, TypeScript, JavaScript (ES6+),
+              Node.js, Redux, RESTful APIs, Firebase, and the Expo framework.
+              Experienced in UX/UI design, Figma, and wire-framing, with a
+              proven track record of collaborating effectively with designers
+              and project managers to deliver high-quality digital products.
             </p>
             <p>
-              Sed euismod, nisl vel ultricies lacinia, nisl nisl aliquam nisl,
-              eget aliquam nisl nisl sit amet nisl. Sed euismod, nisl vel
-              ultricies lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl
-              sit amet nisl.
+              Dynamic and diverse background also includes over 15 years
+              additional experience in retail, food service, freelance writing,
+              and content creation, alongside volunteer roles as a counselor,
+              educator, treasurer, and activist. Native English speaker with
+              conversational proficiency in German (B2.2). Passionate traveler,
+              reader, cinema lover, gamer, sports enthusiast, and committed
+              lifelong learner.
             </p>
           </div>
           <div className={styles.imageContainer}>
             <div className={styles.imageFrame}>
               <Image
-                src="/placeholder.svg?height=400&width=400"
-                alt="Dillon Cleaver"
-                width={400}
-                height={400}
+                src={profilePic}
+                alt="Dillon Cleaver profile picture"
+                width={500}
+                height={300}
                 className={styles.image}
               />
             </div>
